08124e827815ab92f27cd38dd8e1262352433240
Vladislav Kurmaev <Vladikurmav@yandex.ru>
2021-12-12 20:49:24 +0300

    Sprint-14 BuxFix-9

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+// require('dotenv').config();
 const path = require('path');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -22,7 +22,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 const app = express();
-
+// console.log(process.env.NODE_ENV);
 app.use(cors());
 
 app.use(cookieParser());
--- controllers/users.js
+++ controllers/users.js
@@ -5,8 +5,7 @@ const User = require('../models/user');
 const BadRequestError = require('../errors/bad-request-err');
 const NotFoundError = require('../errors/not-found-err');
 const ConflictError = require('../errors/conflict-err');
-
-const { NODE_ENV, JWT_SECRET } = process.env;
+// const { NODE_ENV, JWT_SECRET } = process.env;
 
 const login = (req, res, next) => {
   const { email, password } = req.body;
@@ -15,7 +14,7 @@ const login = (req, res, next) => {
     .then((user) => {
       const token = jwt.sign(
         { _id: user._id },
-        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+        'some-secret-key',
         { expiresIn: '7d' },
       );
       res
